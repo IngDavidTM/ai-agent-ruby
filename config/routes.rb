ed8e96ae70abb8d9079dcd5bd652cae040d2568f
IngDavidTM <david-1011@hotmail.com>
2026-01-09 15:06:10 -0500

    Add a new landing page with a liquid glass design and update root routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'agents#index'
+  root 'pages#home'
+  get 'chat', to: 'agents#index', as: :chat
   post 'agents/chat', to: 'agents#chat'
   delete 'agents/:id', to: 'agents#destroy', as: :delete_conversation
 end
