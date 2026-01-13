49f48d2adda3b89f14dc830d0d79ec2e85867aed
IngDavidTM <david-1011@hotmail.com>
2026-01-13 15:07:00 -0500

    Add a dedicated features page with navigation links and related styling adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about', as: :about
+  get 'features', to: 'pages#features', as: :features
   get 'chat', to: 'agents#index', as: :chat
   post 'agents/chat', to: 'agents#chat'
   delete 'agents/:id', to: 'agents#destroy', as: :delete_conversation
