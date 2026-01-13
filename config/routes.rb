55ddfc8784f72ccca6b97636fc7718561cecfcd1
IngDavidTM <david-1011@hotmail.com>
2026-01-13 14:59:23 -0500

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'about', to: 'pages#about', as: :about
   get 'chat', to: 'agents#index', as: :chat
   post 'agents/chat', to: 'agents#chat'
   delete 'agents/:id', to: 'agents#destroy', as: :delete_conversation
